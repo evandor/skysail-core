4a85d93402300b7ff0e66072970f9159f8583dbe
Carsten Gräf <carsten.graef@kvb.de>
2017-06-20 17:07:54 +0200

    wip

--- skysail.core.akka/src/io/skysail/core/akka/Activator.scala
+++ skysail.core.akka/src/io/skysail/core/akka/Activator.scala
@@ -1,8 +1,21 @@
 package io.skysail.core.akka
 
-import akka.actor.{ Props, ActorSystem }
-import org.osgi.framework.BundleContext
+import scala.concurrent.Future
+
+import akka.actor.{ ActorSystem , Actor, Props }
+import akka.event.Logging
+import akka.util.Timeout
+
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.Http.ServerBinding
+import akka.http.scaladsl.server.Directives._
+import akka.stream.ActorMaterializer
+
+import com.typesafe.config.{ Config, ConfigFactory }
 import akka.osgi.ActorSystemActivator
+import org.osgi.framework.BundleContext
+import scala.concurrent.duration.Duration
+import scala.concurrent.duration.FiniteDuration
 
 class Activator extends ActorSystemActivator {
 
@@ -12,6 +25,30 @@ class Activator extends ActorSystemActivator {
 
     //val someActor = system.actorOf(Props[SomeActor], name = "someName")
     //someActor ! SomeMessage
+
+    implicit val ec = system.dispatcher
+
+    val api = new RestApi(system, requestTimeout()).routes
+
+   // implicit val actorRefFactory = ActorRefFactory()
+    implicit val acctorSystem = ActorSystem()
+    implicit val materializer = ActorMaterializer()
+    val bindingFuture: Future[ServerBinding] = Http().bindAndHandle(api, "localhost", 5001)
+
+    val log = Logging(system.eventStream, "go-ticks")
+    bindingFuture.map { serverBinding =>
+      log.info(s"RestApi bound to ${serverBinding.localAddress} ")
+    }.onFailure {
+      case ex: Exception =>
+        log.error(ex, "Failed to bind to {}:{}!", "localhost", 5001)
+        system.terminate()
+    }
   }
 
+  def requestTimeout(): Timeout = {
+    val t = "1000" //config.getString("akka.http.server.request-timeout")
+    val d = Duration(t)
+    FiniteDuration(d.length, d.unit)
+  }
 }
+
---
+++ skysail.core.akka/test/io/skysail/core/akka/SilentActorTest.scala
@@ -0,0 +1,44 @@
+package io.skysail.core.akka
+
+import org.scalatest.WordSpecLike
+import org.scalatest.MustMatchers
+import akka.testkit.{ TestActorRef, TestKit }
+import akka.actor._
+
+class SilentActorTest extends TestKit(ActorSystem("testsystem"))
+    with WordSpecLike
+    with MustMatchers
+    with StopSystemAfterAll {
+
+  "A Silent Actor" must {
+
+    "change internal state when it receives a message, multi" in {
+      import SilentActor._
+
+      val silentActor = system.actorOf(Props[SilentActor], "s3")
+      silentActor ! SilentMessage("whisper1")
+      silentActor ! SilentMessage("whisper2")
+      silentActor ! GetState(testActor)
+      expectMsg(Vector("whisper1", "whisper2"))
+    }
+
+  }
+
+}
+
+object SilentActor {
+  case class SilentMessage(data: String)
+  case class GetState(receiver: ActorRef)
+}
+
+class SilentActor extends Actor {
+  import SilentActor._
+  var internalState = Vector[String]()
+
+  def receive = {
+    case SilentMessage(data) =>
+      internalState = internalState :+ data
+    case GetState(receiver) => receiver ! internalState
+  }
+}
+
