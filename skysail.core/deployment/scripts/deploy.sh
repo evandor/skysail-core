9f83f89f84eb3c8be6fbb9704ad2268bcf4ef4b8
Carsten Gräf <evandor@gmail.com>
2017-08-12 13:00:28 +0200

    deployment

--- skysail.core/deployment/scripts/deploy.sh
+++ skysail.core/deployment/scripts/deploy.sh
@@ -1,4 +1,4 @@
-JOB_DIR="/home/carsten/.hudson/jobs/ssp.$APPNAME.export.$STAGE/workspace/$PROJECTNAME"
+JOB_DIR="/home/carsten/.hudson/jobs/skysail-core.export.$STAGE/workspace/$PROJECTNAME"
 PRODUCT_DIR="/home/carsten/skysail/products/$APPNAME/$STAGE"
 SERVICENAME=${APPNAME}_${STAGE}
 export JAVA_HOME=/home/carsten/.hudson/tools/hudson.model.JDK/java_SDK_8u25/
--- skysail.core/deployment/scripts/startService.sh
+++ skysail.core/deployment/scripts/startService.sh
@@ -3,6 +3,13 @@ PRODUCT_DIR="/home/carsten/skysail/products/$APPNAME/$STAGE"
 SERVICENAME=${APPNAME}_${STAGE}
 export JAVA_HOME=/home/carsten/.hudson/tools/hudson.model.JDK/java_SDK_8u25/
 
+echo "PROJECTNAME: $PROJECTNAME"
+echo "APPNAME:     $APPNAME"
+echo "STAGE:       $STAGE"
+echo "JOB_DIR:     $JOB_DIR"
+echo "PRODUCT_DIR: $PRODUCT_DIR"
+echo "SERVICENAME: $SERVICENAME"
+
 ### STOPPING SERVICE #####################################################
 echo ""
 echo "Stopping Service:"
@@ -21,6 +28,7 @@ echo ""
 echo "Preparing Service:"
 echo "------------------"
 
+echo "cd $JOB_DIR/generated/distributions/executable"
 cd $JOB_DIR/generated/distributions/executable
 
 echo "copying $APPNAME.jar to $PRODUCT_DIR/bin/$APPNAME.jar"
