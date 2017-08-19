705a200930b50b8a726efb01a072b3dddf19ab5e
Carsten Gräf <evandor@gmail.com>
2017-08-19 09:39:41 +0200

    wip

--- skysail.core/client/src/app/app-routing.module.ts
+++ skysail.core/client/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppsComponent}     from './apps/apps.component';
+import { BundlesComponent } from './bundles/bundles.component'
  
 const routes: Routes = [
   { path: '', redirectTo: '/services', pathMatch: 'full' },
+  { path: 'bundles', component: BundlesComponent },
   { path: 'services', component: AppsComponent }
 ];
  
--- skysail.core/client/src/app/app.module.ts
+++ skysail.core/client/src/app/app.module.ts
@@ -5,18 +5,34 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AppsComponent } from './apps/apps.component';
 import { AppRoutingModule }     from './app-routing.module';
+import { BackendService } from './services/backend.service';
+import { BundlesComponent } from './bundles/bundles.component';
+
+import { AccordionModule, TabViewModule, MenuModule, MenubarModule,DataTableModule,SharedModule } from 'primeng/primeng';
+import { HttpModule } from '@angular/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    AppsComponent
+    AppsComponent,
+    BundlesComponent
   ],
   imports: [
+    BrowserModule,
+//    FormsModule,
+    HttpModule,
+    AccordionModule,
+    TabViewModule,
+    MenuModule,
+    MenubarModule,
+    DataTableModule,SharedModule,
+    AppRoutingModule,
+//    CommonModule,
     AppRoutingModule,
     BrowserModule
   ],
-  providers: [],
+  providers: [BackendService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
