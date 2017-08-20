2f6dfe0f396c14ba170be5f055a87834d6b7492a
Carsten Graef <evandor@gmail.com>
2017-08-20 10:31:51 +0200

    wip

--- skysail.core/client/src/app/navbar/navbar.component.ts
+++ skysail.core/client/src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MenubarModule, MenuItem } from 'primeng/primeng';
 
 @Component({
   selector: 'navbar',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  items: MenuItem[];
+
   constructor() { }
 
   ngOnInit() {
+    this.items = [
+      {
+        label: 'Bundles',
+        icon: 'fa-th-large',
+        url: '/bundles'
+      },
+      {
+        label: 'Services',
+        icon: 'fa-play-circle',
+        url: '/services'
+      }
+    ];
   }
 
 }
